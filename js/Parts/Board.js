0e43baa6468076c57557a35b136c7fb9b86e5990
Nancy Agapito Lacsamana <nla031@post.uit.no>
2021-11-02 20:44:20 +0100

    New functions: - addPhysicsAmmo: to add rigid physics on an object - createExtrudeMesh: to create an extrude mesh with or without a hole.

--- js/Parts/Board.js
+++ js/Parts/Board.js
@@ -6,9 +6,15 @@ import {commons} from "../../../PinBall/lib/ammohelpers/lib/Common.js";
  * MERK2: Bruker btBvhTriangleMeshShape (og ikke btBvhConvexTriangleMeshShape) til planet.
  */
 export const board= {
+    //Physics Ammo
     myPhysicsWorld: undefined,
+    //Rigid bodies for physics
     gameBoardRigidBody: undefined,
     bottomBoardRigidBody: undefined,
+    coverBoardRigidBody: undefined,
+    startRampRigidBody: undefined,
+    diagonalBarBody: undefined,
+
     TERRAIN_SIZE: 100,
 
 
@@ -28,18 +34,14 @@ export const board= {
         groupMesh.position.set(position.x, position.y, position.z);
 
         //FRAME
-        let frameShape = this.createThreeShape(this.TERRAIN_SIZE*2, this.TERRAIN_SIZE*3);
         let hole1 = new THREE.Shape();
         hole1.moveTo( 5,5 );
         hole1.lineTo( 5, this.TERRAIN_SIZE * 3 -5 );
         hole1.lineTo( this.TERRAIN_SIZE * 2-5, this.TERRAIN_SIZE * 3-5 );
         hole1.lineTo( this.TERRAIN_SIZE * 2-5, 5 );
         hole1.lineTo( 5, 5 );
-        frameShape.holes.push(hole1);
-        let frameExtrudeSettings = {steps: 1,depth: 15,bevelEnabled: true,bevelThickness: 1,bevelSize: 1,bevelOffset: 0,bevelSegments: 1};
-        let frameBoardGeometry = new THREE.ExtrudeGeometry( frameShape, frameExtrudeSettings );
         let frameBoardMaterial = new THREE.MeshPhongMaterial( { color: 0x3d85c6, side: THREE.DoubleSide } );
-        let frameBoardMesh = new THREE.Mesh( frameBoardGeometry, frameBoardMaterial );
+        let frameBoardMesh = this.createExtrudeMesh(this.TERRAIN_SIZE*2, this.TERRAIN_SIZE*3, 1, 15, true, 1, 1,0,1,frameBoardMaterial, hole1);
         frameBoardMesh.rotation.x = -Math.PI / 2;
         frameBoardMesh.position.x = -this.TERRAIN_SIZE;
         frameBoardMesh.position.z = this.TERRAIN_SIZE;
@@ -47,11 +49,8 @@ export const board= {
         groupMesh.add( frameBoardMesh );
 
         //BOTTOM
-        let bottomBoardShape = this.createThreeShape(this.TERRAIN_SIZE*2, this.TERRAIN_SIZE*3);
-        let bottomExtrudeSettings = {steps: 1,depth: 5,bevelEnabled: true,bevelThickness: 1,bevelSize: 1,bevelOffset: 0,bevelSegments: 1};
-        let bottomBoardGeometry = new THREE.ExtrudeGeometry( bottomBoardShape, bottomExtrudeSettings );
         let bottomBoardMaterial = new THREE.MeshPhongMaterial( { color: 0x121212, side: THREE.DoubleSide } );
-        let bottomBoardMesh = new THREE.Mesh(bottomBoardGeometry, bottomBoardMaterial);
+        let bottomBoardMesh = this.createExtrudeMesh(this.TERRAIN_SIZE*2,this.TERRAIN_SIZE*3, 1, 5, true, 1,1, 0, 1, bottomBoardMaterial);
         bottomBoardMesh.rotation.x = -Math.PI / 2;
         bottomBoardMesh.position.x = -this.TERRAIN_SIZE;
         bottomBoardMesh.position.z = this.TERRAIN_SIZE;
@@ -59,14 +58,11 @@ export const board= {
         groupMesh.add( bottomBoardMesh );
 
         /**************Start: No Ammos Yet!********************************/
-            //GLASS COVER; Ingen AMMO, dvs ballen kan komme gjennom
-        let coverBoardShape = this.createThreeShape(this.TERRAIN_SIZE*2, this.TERRAIN_SIZE*3);
-        let coverExtrudeSettings = {steps: 1,depth: 0.3, bevelEnabled: true,bevelThickness: 1,bevelSize: 1,bevelOffset: 0,bevelSegments: 1};
-        let coverBoardGeometry = new THREE.ExtrudeGeometry( coverBoardShape, coverExtrudeSettings );
+        //GLASS COVER; Ingen AMMO, dvs ballen kan komme gjennom
         let coverBoardMaterial = new THREE.MeshPhongMaterial( { color: 0xeeeeee, side: THREE.DoubleSide } );
         coverBoardMaterial.transparent = true;
         coverBoardMaterial.opacity = 0.1;
-        let coverBoardMesh = new THREE.Mesh(coverBoardGeometry, coverBoardMaterial);
+        let coverBoardMesh = this.createExtrudeMesh(this.TERRAIN_SIZE*2, this.TERRAIN_SIZE*3, 1, 0.3, true, 1, 1, 0, 1, coverBoardMaterial);
         coverBoardMesh.rotation.x = -Math.PI / 2;
         coverBoardMesh.position.x = -this.TERRAIN_SIZE;
         coverBoardMesh.position.z = this.TERRAIN_SIZE;
@@ -75,11 +71,8 @@ export const board= {
         groupMesh.add( coverBoardMesh );
 
         //RAMP on the right
-        let startRampShape = this.createThreeShape(10, this.TERRAIN_SIZE*3 -70);
-        let startRampExtrudeSettings = {steps: 1,depth: 15,bevelEnabled: true,bevelThickness: 1,bevelSize: 1,bevelOffset: 0,bevelSegments: 1};
-        let startRampGeo = new THREE.ExtrudeGeometry( startRampShape, startRampExtrudeSettings);
         let startRampMaterial = new THREE.MeshPhongMaterial( { color: 0xF31CEC, side: THREE.DoubleSide } );
-        let startRampMesh = new THREE.Mesh(startRampGeo, startRampMaterial);
+        let startRampMesh = this.createExtrudeMesh(10, this.TERRAIN_SIZE*3-70, 1, 15, true, 1,1,0, 1, startRampMaterial);
         startRampMesh.rotation.x = -Math.PI / 2;
         startRampMesh.position.x = this.TERRAIN_SIZE - 30;
         startRampMesh.position.z = this.TERRAIN_SIZE - 5;
@@ -87,11 +80,8 @@ export const board= {
         groupMesh.add(startRampMesh);
 
         //RAMP on top right
-        let diagonalRampShape = this.createThreeShape(5, 94.2);
-        let diagonalRampExtrudeSettings = {steps: 1,depth: 14,bevelEnabled: true,bevelThickness: 1,bevelSize: 1,bevelOffset: 0,bevelSegments: 1};
-        let diagonalRampGeo = new THREE.ExtrudeGeometry( diagonalRampShape, diagonalRampExtrudeSettings);
         let diagonalRampMaterial = new THREE.MeshPhongMaterial( { color: 0xF5571F, side: THREE.DoubleSide } );
-        let diagonalRampMesh = new THREE.Mesh(diagonalRampGeo, diagonalRampMaterial);
+        let diagonalRampMesh = this.createExtrudeMesh(5, 94.2, 1,14,true, 1, 1, 0,1, diagonalRampMaterial);
         diagonalRampMesh.rotation.x = -Math.PI / 2;
         diagonalRampMesh.rotation.z = 1.05;
         diagonalRampMesh.position.x = this.TERRAIN_SIZE -2.3;
@@ -101,43 +91,35 @@ export const board= {
         /*************End: No ammos.*************************************/
 
         /******ACTIVATE WHEN BOARD IS FINISHED*******/
-        //groupMesh.rotation.x = Math.PI/2;
+            //groupMesh.rotation.x = 0.2;
 
 
-        //AMMO: frame
+        //PhysicsAmmo: frame
         let compoundShape = new Ammo.btCompoundShape();
         commons.createTriangleShapeAddToCompound(compoundShape, frameBoardMesh);
         this.gameBoardRigidBody = commons.createAmmoRigidBody(compoundShape, groupMesh, 0.05, 0.3, position, mass);
-        // BODYFLAG_KINEMATIC_OBJECT = 2 betyr kinematic object, masse=0 men kan flyttes!!
         this.gameBoardRigidBody.setCollisionFlags(this.gameBoardRigidBody.getCollisionFlags() | 2);
-        // Never sleep, BODYSTATE_DISABLE_DEACTIVATION = 4
         this.gameBoardRigidBody.setActivationState(4);
+        this.addPhysicsAmmo(this.gameBoardRigidBody, groupMesh, setCollisionMask);
 
-        this.myPhysicsWorld.addPhysicsObject(
-            this.gameBoardRigidBody,
-            groupMesh,
-            setCollisionMask,
-            this.myPhysicsWorld.COLLISION_GROUP_PLANE,
-            this.myPhysicsWorld.COLLISION_GROUP_SPHERE |
-            this.myPhysicsWorld.COLLISION_GROUP_COMPOUND |
-            this.myPhysicsWorld.COLLISION_GROUP_MOVEABLE |
-            this.myPhysicsWorld.COLLISION_GROUP_CONVEX |
-            this.myPhysicsWorld.COLLISION_GROUP_TRIANGLE
-        );
-
-        //AMMO: bottom
+        //PhysicsAmmo: bottom
         let bottomCompoundShape = new Ammo.btCompoundShape();
         commons.createTriangleShapeAddToCompound(bottomCompoundShape, bottomBoardMesh);
         this.bottomBoardRigidBody = commons.createAmmoRigidBody(bottomCompoundShape, groupMesh, 0.05, 0.3, position, mass);
-        // BODYFLAG_KINEMATIC_OBJECT = 2 betyr kinematic object, masse=0 men kan flyttes!!
         this.bottomBoardRigidBody.setCollisionFlags(this.bottomBoardRigidBody.getCollisionFlags() | 2);
-        // Never sleep, BODYSTATE_DISABLE_DEACTIVATION = 4
         this.bottomBoardRigidBody.setActivationState(4);
+        this.addPhysicsAmmo(this.bottomBoardRigidBody, groupMesh, setCollisionMask);
 
+
+    },
+
+
+
+    addPhysicsAmmo(rigidBody, groupMesh, collisionMask){
         this.myPhysicsWorld.addPhysicsObject(
-            this.bottomBoardRigidBody,
+            rigidBody,
             groupMesh,
-            setCollisionMask,
+            collisionMask,
             this.myPhysicsWorld.COLLISION_GROUP_PLANE,
             this.myPhysicsWorld.COLLISION_GROUP_SPHERE |
             this.myPhysicsWorld.COLLISION_GROUP_COMPOUND |
@@ -145,7 +127,6 @@ export const board= {
             this.myPhysicsWorld.COLLISION_GROUP_CONVEX |
             this.myPhysicsWorld.COLLISION_GROUP_TRIANGLE
         );
-
     },
 
     createThreeShape(length, width) {
@@ -158,6 +139,24 @@ export const board= {
         shape.lineTo( length, 0 );
         shape.lineTo( 0, 0 );
         return shape;
+    },
+
+    createExtrudeMesh(length, width, steps, depth, bevelEnabled, bevelThickness, bevelSize, bevelOffset, bevelSegments, material, hole){
+        let shape = this.createThreeShape(length, width);
+        if (hole){
+            shape.holes.push(hole);
+        }
+        let extrudeSettings = {
+            steps: steps,
+            depth: depth,
+            bevelEnabled: bevelEnabled,
+            bevelThickness: bevelThickness,
+            bevelSize: bevelSize,
+            bevelOffset: bevelOffset,
+            bevelSegments: bevelSegments};
+        let shapeGeometry = new THREE.ExtrudeGeometry( shape, extrudeSettings );
+        let extrudeMesh = new THREE.Mesh(shapeGeometry, material);
+        return extrudeMesh;
     },
 
     makeSimpleBoxMesh(width, height, depth, material){
